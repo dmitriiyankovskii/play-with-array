82c0cfeaa64e7108aa202c8f7d5fc877e22a4523
dmitriiyankovskii <iankovskii.ds@phystech.edu>
2024-08-27 16:40:04 +0300

    addition matrix

--- array.cpp
+++ array.cpp
@@ -1,41 +1,48 @@
 #include <stdio.h>
-const int ROWS = 5;
-const int COLS = 4;
 
-void print(int *);
-void print2(int [][COLS], int);
-void print3(int [][COLS]);
+const int COLS = 10;
+
+void addition(int (*arr1)[COLS], int ,  int (*arr2)[COLS], int (*arr3)[COLS]);
+void print(int (*arr)[COLS], int);
+void print_p(int (*arr)[COLS], int);
+void print_p(int (*arr)[COLS], int, int);
 
 int main(void) {
-    //int arr1[5] = {1, 2, 3, 4, 5};
-    int arr[ROWS][COLS] = {
-        {2, 45, 23, 53},
-        {33, 78, 12, 0},
-        {55, 67, 22, 5},
-        {345, 234, 432, 659},
-        {213, 324, 9054, 332}
-    };
     
-    //print(arr1);
-    //print2(arr, ROWS);
-    print3(arr);
-}
 
-void print(int *array) {
-    for(int i = 0; i < 5; i++) {
-        printf("arr[%d] = %d\n", i, array[i]);
-    }
-}
+    int matrix1[2][COLS] = { {1, 4, 5, 3, 7, 4, 2, 5, 6, 6},
+                          {5, 2, 7, 3, 5, 9, 6, 3, 5, 3} };
+    int matrix2[2][COLS] = { {8, 5, 3, 7, 3, 6, 1, 3, 6, 3},
+                          {4, 5, 2, 6, 4, 3, 6, 7, 5, 6} };
+    int arr3[2][COLS];
 
-void print2(int data[][COLS], int rows) {
-    for(int i = 0; i < rows; i++) {
+    addition(matrix1, 2, matrix2, arr3);
+    print(matrix1, 2);
+    print(matrix2, 2);
+    print(arr3, 2);
+    print_p(matrix1, 1, 5);
+}
+void addition(const int (*first_matrix)[COLS], int row1, const int (*second_matrix)[COLS], int (*result_matrix)[COLS]){
+    for(int i = 0; i < row1; i++) {
         for(int j = 0; j < COLS; j++) {
-            printf("arr[%d][%d] = %d\n", i, j, data[i][j]);
+            arr3[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+
+    
+}
+
+void print(const int (*arr)[COLS], int row1) {
+    for(int i = 0; i < row1; i++) {
+        for(int j = 0; j < COLS; j++) {
+            printf("%3d ", arr[i][j]);
         }
         printf("\n");
     }
 }
 
-void print3(int data[][COLS]) {
-    printf("%d", *(*(data + 3) + 3));
+void print_p(const int (*arr)[COLS], int i, int j){
+    int x = *(*arr + i*COLS + j);
+    printf("%d", x);
 }
+
